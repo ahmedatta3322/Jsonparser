195d5df3fa0f92f750ebe18b248fab2d7e9ffa44
ahmed atta <ahmedatta3322@gmail.com>
2021-04-07 21:07:00 +0200

    Deal with list of vehicles condition

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta ,abstractmethod
-import sys , json , xmltodict
+import sys , json , xmltodict , csv
 import xml.etree.ElementTree as ET
 #define the main abstraction class
 class IConvert(metaclass=ABCMeta):
@@ -18,26 +18,33 @@ class XmlParser(IConvert):
     def __init__(self,file):
         #define the inputs
         self.file = file
-    #import the data and parse it to dict
+#import the data and parse it to dict
     def importdata(self):
         xmlcontent = open(self.file).read()
         jsoncontent = xmltodict.parse(xmlcontent) 
         return jsoncontent
-    #convert the dict to json
+#convert the dict to json
     def convert(self):
         jsoncontent = self.importdata()
         jsoncontent['file_name'] = str('xml/' + self.file)
-        #convert main fields to small case
+#convert main fields to small case
         jsoncontent['transaction'] ,jsoncontent['transaction']["customer"] ,jsoncontent['transaction']["date"] , jsoncontent['Transaction']['Customer']['id'] = jsoncontent['Transaction'] , jsoncontent['Transaction']['Customer'] ,jsoncontent['Transaction']["Date"],jsoncontent['Transaction']['Customer']['@id']
-        #convert unites to vehicles list 
+#convert unites to vehicles list 
         jsoncontent['transaction']["vehicles"] = []
         for key,value in jsoncontent['transaction']['Customer']['Units'].items():  
-            value['id'] ,value['make'] ,value['vinNumber'] = value['@id'] , value['Make'] ,value['VinNumber']
-            del value['@id'] , value['VinNumber'] , value['Make']
-            jsoncontent['transaction']["vehicles"].append(dict(value))
+            if(isinstance(value,dict)):
+                value['id'] ,value['make'] ,value['vinNumber'] = value['@id'] , value['Make'] ,value['VinNumber']
+                del value['@id'] , value['VinNumber'] , value['Make']
+                jsoncontent['transaction']["vehicles"].append(dict(value))
+            #Deal with list of vehicles
+            elif(isinstance(value,list)):
+                for element in value:
+                    element['id'] ,element['make'] ,element['vinNumber'] = element['@id'] , element['Make'] ,element['VinNumber']
+                    del element['@id'] , element['VinNumber'] , element['Make']
+                    jsoncontent['transaction']["vehicles"].append(dict(element))
         del jsoncontent['Transaction'] , jsoncontent['transaction']['Customer']['Units'] , jsoncontent['transaction']['Customer'] ,jsoncontent['transaction']["Date"],jsoncontent['transaction']['customer']['@id']
         return jsoncontent
-    #Export converted json
+#Export converted json
     def exportdata(self):
         jsoncontent = self.convert()
         out_file = open("parsing_result/sample.json", "w")
@@ -49,19 +56,28 @@ class CsvParser(IConvert):
         #define the inputs , vfile for vehicles , cfile for customers
         self.cfile = cfile
         self.vfile = vfile
-    def convert(self):
+    def importdata(self):
+        with open(cfile) as csvcontent:
+            csv_reader = csv.reader(csvcontent, delimiter = ',')
+            for row in csv_reader:
+                print(row)
         
+    def convert(self):
         pass
+    def exportdata(self):
+        jsoncontent = self.convert()
+        out_file = open("parsing_result/sample.json", "w")
+        res = json.dump(jsoncontent,out_file)
 #define the main creator of the Json "factory" 
 class JsonCreator:
     @staticmethod
     def convert_to_json(thetype):
         if thetype == 'xml':
             #pass the files from the cml
-            return XmlParser("input_data/" + sys.argv[2]).exportdata()
+            return XmlParser("input_data/xml/" + sys.argv[2]).exportdata()
         elif thetype == 'csv':
             #pass the files from the cml
-            return CsvParser(sys.argv[2],sys.argv[3]).convert
+            return CsvParser("input_data/csv/" + sys.argv[2],sys.argv[3]).convert
         else :
             return None
 if __name__ == "__main__":
